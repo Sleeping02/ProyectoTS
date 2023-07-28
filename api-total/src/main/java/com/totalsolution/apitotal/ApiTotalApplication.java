58ed9be641164a399ca0e6d18d1df2046ecfa995
Angelo <chise_elunico@hotmail.com>
2023-07-28 18:51:05 -0500

    Creación de proyecto

---
+++ api-total/src/main/java/com/totalsolution/apitotal/ApiTotalApplication.java
@@ -0,0 +1,14 @@
+package com.totalsolution.apitotal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTotalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiTotalApplication.class, args);
+		System.out.println("Holaa amujndsad");
+	}
+
+}
---
+++ api-total/src/test/java/com/totalsolution/apitotal/ApiTotalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.totalsolution.apitotal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTotalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
